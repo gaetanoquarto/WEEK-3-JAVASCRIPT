21bc000acec24cf173f4c0389c4033cd2ca83d66
gaetanoquarto <114237045+gaetanoquarto@users.noreply.github.com>
2022-10-10 15:09:59 +0200

    Add files via upload

---
+++ 1.Introduzione-Javascript/start/assets/js/index.js
@@ -0,0 +1,3 @@
+window.alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
+window.alert ('è tutto molto bello');
---
+++ 1.Introduzione-Javascript/start/assets/js/naming.js
@@ -0,0 +1,22 @@
+var pet_preferito = 'gatto';
+console.log(pet_preferito);
+
+var pet = 'gatto';
+var Pet = 'cane';
+console.log(Pet, pet);
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+document.getElementById('petpreferito').innerHTML += (petPreferito);
+
+//var numero = 12;
+var numero = 'il numero 12';
+
+document.getElementById('numerovincente').innerHTML += (numero);
